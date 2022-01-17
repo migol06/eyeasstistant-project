6462260744f8c425f8394b90cf95a71f51385e46
Miguel Edward A. Rosal <miguelrosal@exploretale.com>
2022-01-17 13:05:15 +0800

    Object Detection Working

--- lib/screens/livecamera_screen.dart
+++ lib/screens/livecamera_screen.dart
@@ -1,8 +1,8 @@
 import 'package:camera/camera.dart';
 import 'package:eyeassistant/main.dart';
-import 'package:eyeassistant/widgets/app_bar.dart';
-import 'package:eyeassistant/widgets/text.dart';
+import 'package:eyeassistant/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:tflite/tflite.dart';
 
 class ESLiveCameraScreen extends StatefulWidget {
   const ESLiveCameraScreen({Key? key}) : super(key: key);
@@ -14,6 +14,15 @@ class ESLiveCameraScreen extends StatefulWidget {
 class _ESLiveCameraScreenState extends State<ESLiveCameraScreen> {
   CameraController? controller;
   bool isDetecting = false;
+  String detectedImage = '';
+  int endTime = 0;
+
+  loadModel() async {
+    String? res = await Tflite.loadModel(
+        model: "assets/model/mobilenet_v1_1.0_224.tflite",
+        labels: "assets/model/mobilenet_v1_1.0_224.txt");
+    print(res);
+  }
 
   @override
   void initState() {
@@ -22,20 +31,48 @@ class _ESLiveCameraScreenState extends State<ESLiveCameraScreen> {
       if (!mounted) {
         return;
       }
-      setState(() {});
+      setState(() {
+        controller?.startImageStream((CameraImage img) async {
+          if (!isDetecting) {
+            isDetecting = true;
+            int startTime = DateTime.now().microsecondsSinceEpoch;
+            var recognitions = await Tflite.runModelOnFrame(
+                bytesList: img.planes.map((plane) {
+                  return plane.bytes;
+                }).toList(),
+                imageHeight: img.height,
+                imageWidth: img.width,
+                imageMean: 127.5,
+                imageStd: 127.5,
+                rotation: 90,
+                numResults: 1,
+                threshold: 0.1,
+                asynch: true);
+            recognitions?.forEach((response) {
+              detectedImage += response['label'] +
+                  ' ' +
+                  (response['confidence'] as double).toStringAsFixed(2);
+              int endTime = DateTime.now().millisecondsSinceEpoch;
+            });
+            setState(() {
+              detectedImage;
+              print(detectedImage);
+              print('Detection took ${endTime - startTime}');
+            });
+
+            isDetecting = false;
+          }
+        });
+      });
     });
 
-    controller?.startImageStream((CameraImage img) {
-      if (!isDetecting) {
-        isDetecting = true;
-        int startTime = DateTime.now().microsecondsSinceEpoch;
-      }
-    });
+    loadModel();
     super.initState();
   }
 
   @override
-  void dispose() {
+  void dispose() async {
+    await Tflite.close();
     controller?.dispose();
     super.dispose();
   }
@@ -55,7 +92,10 @@ class _ESLiveCameraScreenState extends State<ESLiveCameraScreen> {
               children: [const ESText('Lorem Ipsum Dolor')]);
         },
       ),
-      body: Center(child: CameraPreview(controller!)),
+      body: Stack(children: [
+        Center(child: CameraPreview(controller!)),
+        ESText(detectedImage)
+      ]),
     );
   }
 }
